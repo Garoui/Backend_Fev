35ec5aa850c2eb024db44a2124039b6f61cfcf5d
Garoui <118277240+Garoui@users.noreply.github.com>
2025-02-09 20:46:55 +0100

    importation les routes,configuration dot env

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const {connectToMongoDb} = require("./config/db");
+require("dotenv").config();
 const http = require('http');//1
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/indexRouter');
+var usersRouter = require('./routes/usersRouter');
+var osRouter = require('./routes/osRouter');
 
 var app = express();
 
@@ -19,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/os', osRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -36,8 +39,9 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-const server = http.createServer(app);//2
-server.listen(4000, () => {
-  console.log("app is running on port 4000");
+const server = http.createServer(app); //2
+server.listen(process.env.port, () => {
+  connectToMongoDb()
+  console.log("app is running on port 5000");
 
 });
